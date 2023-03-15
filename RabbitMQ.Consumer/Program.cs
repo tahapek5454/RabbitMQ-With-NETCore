c67b6997b135a7d8e93d53827e9d4c5e0ef0bb94
tahapek5454 <tahapek5454@gmail.com>
2023-03-15 20:04:28 +0300

    feat: add connections

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Publisher;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+
+// baglanti islemi
+factory.Uri = new(MyConfiguration.GetResponse());
+
+// baglanti aktiflestirme ve kanal acma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// queue olusturma
+channel.QueueDeclare(queue: "example-queue", exclusive: false); // consumerla baglanmak amaciyla exclusive false verdik
+// consumerdaki kuyruk publisherla ayni olmak zorunda
+
+// queue'dan mesaj okuma
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue", false, consumer: consumer); // ne zaman kuyruktaki consumera bilgi gelir consume et
+// receive bir delegate
+consumer.Received += (sender, e) =>
+{
+    // kuyruga gelen mesajın islendigi yerdir
+
+    // e.body = kuyruktkai mesajın verisini butunsel olarak getirir
+    // e.body.span ya da e.body.ToArray() verisini byte olarak getirir.
+
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+
+};
+
+Console.ReadLine();
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Publisher;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+
+// baglanti islemi
+factory.Uri = new(MyConfiguration.GetResponse());
+
+// baglanti aktiflestirme ve kanal acma
+using IConnection connection= factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// queue olusturma
+channel.QueueDeclare(queue: "example-queue", exclusive:false); // consumerla baglanmak amaciyla exclusive false verdik
+
+// queueya mesaj gonderme
+// rabbitMQ queue ya atilan mesajlari byte kabul eder.
+
+byte[] message = Encoding.UTF8.GetBytes("merhaba");
+
+channel.BasicPublish(exchange: "", routingKey:"example-queue", body: message); // exchange bos verirsen default exchane = direct exchange
+
+Console.ReadLine();
