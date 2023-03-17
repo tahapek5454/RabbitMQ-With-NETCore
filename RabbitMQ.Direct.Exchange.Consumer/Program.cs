0c31bbb0dba00cb65902cdb8a2b7b83993d0aa7d
tahapek5454 <tahapek5454@gmail.com>
2023-03-17 10:23:55 +0300

    feat: add direct exchange behavior

---
+++ RabbitMQ.Direct.Exchange.Consumer/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Direct.Exchange.Consumer;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+
+// baglanti islemi
+factory.Uri = new(MyConfiguration.GetResponse());
+
+// baglanti aktiflestirme
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// Ust taraf Klasik
+
+// publisherdaki exhange ile birebir aynı isme ve type sahip bir exhange tanımlanmaktır
+// exchange olusturma islemi
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+
+// exhangein routing ket ile belirledigi kuyrugu olusutmalisin
+
+
+
+// publisher tarafından routing key de bulunan değerdeki kuyruğa gönderilen mesajları
+// kendi oluşturduğumuz kuyruga yonlendirerek tuketmemiz gerekmektedir bunu için oncelikle
+// bir kuyruk olusturulmalıdır
+
+//random isimli default ayarli queue olusturur ve bize bunun namei lazım
+string queueName = channel.QueueDeclare().QueueName;
+
+
+// simdi ise QueBinding islemi yapacagiz
+
+channel.QueueBind(queue: queueName, exchange: "direct-exchange-example", routingKey: "direct-queue-example");
+// routing key degerine karsilik olarak gonderilen mesajlari queue ya yonlendirmis oluyoruz queuda exchange ile bind ediliyor(Direct exchange)
+
+
+// tuketim islemi
+EventingBasicConsumer consumer = new(channel);
+
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.ReadLine();
+
+
---
+++ RabbitMQ.Direct.Exchange.Producer/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Direct.Exchange.Producer;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+
+// baglanti islemi
+factory.Uri= new (MyConfiguration.GetResponse());
+
+// baglanti aktiflestirme
+using IConnection connection= factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// Ust taraf Klasik
+
+// exchange olusturma islemi
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+// exhange olusturdugunda ayriyetten bir queue olusturmuyorsun, queue yu consumer olusturuyor
+
+while (true)
+{
+    Console.Write("Mesaj Giriniz: ");
+    string? message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+    // yayınlama islemi
+    channel.BasicPublish(
+        exchange: "direct-exchange-example",
+        routingKey: "direct-queue-example",
+        body: byteMessage
+    
+        );
+}
+
