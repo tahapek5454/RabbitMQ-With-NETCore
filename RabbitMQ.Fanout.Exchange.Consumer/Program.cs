5de1f451558a31f78759aa71e4316d2b603e9d1e
tahapek5454 <tahapek5454@gmail.com>
2023-03-18 15:19:13 +0300

    feat: add fanout exchange

---
+++ RabbitMQ.Fanout.Exchange.Consumer/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Fanout.Exchange.Consumer;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+
+// baglanti islemi
+factory.Uri = new(MyConfiguration.GetResponse());
+
+// baglanti aktiflestirme
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// exchange olusturma
+channel.ExchangeDeclare(exchange: "fanout-exchange-example", type: ExchangeType.Fanout);
+
+Console.WriteLine("Kuyruk Adini Giriniz: ");
+string _queueName = Console.ReadLine();
+
+// kuyruk olusturma
+channel.QueueDeclare(queue:_queueName, exclusive:false);
+
+// binding islemi
+// queue ile exchange i rounting key uzerinden bind etmis oluyoruz
+channel.QueueBind(queue: _queueName, exchange: "fanout-exchange-example", routingKey: string.Empty);
+
+// mesajlari okuma
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: _queueName, autoAck: true, consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+
+};
+Console.ReadLine();
---
+++ RabbitMQ.Fanout.Exchange.Producer/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Fanout.Exchange.Producer;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+
+// baglanti islemi
+factory.Uri = new(MyConfiguration.GetResponse());
+
+// baglanti aktiflestirme
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// exchange olusturma
+channel.ExchangeDeclare(exchange: "fanout-exchange-example", type: ExchangeType.Fanout);
+
+
+// mesaj yayımlama
+
+for(int i = 0;i < 100; i++)
+{
+    await Task.Delay(200);
+
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+
+    //fanout exhange de herhangi bir kuyrugu ayirt etmeyecegimizden dolayi routing key e bos deger veriyoruz
+    channel.BasicPublish(exchange: "fanout-exchange-example", routingKey: string.Empty, body:message);
+}
+
+Console.ReadLine();
