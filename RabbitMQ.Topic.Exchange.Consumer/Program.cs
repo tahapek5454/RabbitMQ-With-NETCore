43e132c36132b2cf5fb90ef96d7f590175bfa778
tahapek5454 <tahapek5454@gmail.com>
2023-03-18 15:55:42 +0300

    feat: add topic exchange

---
+++ RabbitMQ.Topic.Exchange.Consumer/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Topic.Exchange.Consumer;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+
+// baglanti islemi
+factory.Uri = new(MyConfiguration.GetResponse());
+
+// baglanti aktiflestirme
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//exchange belirleme
+
+channel.ExchangeDeclare(exchange: "topic-exchange-example", type: ExchangeType.Topic);
+
+Console.Write("Dinlenecek Topic i giriniz ");
+string topic = Console.ReadLine();
+
+// queue olusturma o kendisi name atsin biz o nami alalim
+string queueName = channel.QueueDeclare().QueueName;
+
+//binding
+
+channel.QueueBind(queue: queueName, exchange: "topic-exchange-example", routingKey: topic);
+
+//MESAJ
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+
+};
+
+Console.ReadLine();
---
+++ RabbitMQ.Topic.Exchange.Producer/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Topic.Exchange.Producer;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+
+// baglanti islemi
+factory.Uri = new(MyConfiguration.GetResponse());
+
+// baglanti aktiflestirme
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//exchange belirleme
+
+channel.ExchangeDeclare(exchange: "topic-exchange-example", type: ExchangeType.Topic);
+
+// mesaj gonderme
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba " + i);
+
+    // hangi topic e gonderecegimizi kullanicidan alicaz
+    Console.Write("Mesajin gonderilecegi Topici Giriniz : ");
+    string topic = Console.ReadLine();
+    // hangi topice gore islem belirleyecegimizi routing key e bildiriyoruz
+    channel.BasicPublish(
+        exchange: "topic-exchange-example",
+        routingKey: topic,
+        body: message
+        );
+
+}
+
+Console.ReadLine();
